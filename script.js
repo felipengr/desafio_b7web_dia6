d2fbd23d7ab498efec83d01df534c453e42baedd
Felipe Nogueira <felipenogueira.94@gmail.com>
2021-07-24 21:46:06 -0300

    js complete

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // Inital Data
 let currentQuestion = 0;
+let correctAnswers = 0;
 
 showQuestion();
+
+// Events
+
+document.querySelector('.scoreArea button').addEventListener('click', resetEvent)
 
 // Functions
 function showQuestion() {
     if(questions[currentQuestion]) {
         let q = questions[currentQuestion];
+
+        let pct = Math.floor((currentQuestion / questions.length) * 100);
+        document.querySelector('.progress--bar').style.width = `${pct}%`;
 
         document.querySelector('.scoreArea').style.display = 'none';
         document.querySelector('.questionArea').style.display = 'block';
@@ -23,7 +31,7 @@ function showQuestion() {
         });
 
     } else {
-
+        finishQuiz();
     }
 };
 
@@ -31,8 +39,37 @@ function optionClickEvent (e) {
     let clickedOption = parseInt(e.target.getAttribute('data-op'));
 
     if(questions[currentQuestion].answer === clickedOption) {
-
-    } else {
-        
+        correctAnswers++;
     }
+    
+    currentQuestion++;
+    showQuestion();
+}
+
+function finishQuiz () {
+    let points = Math.floor((correctAnswers / questions.length) * 100);
+
+    if (points < 50) {
+        document.querySelector('.scoreText1').innerHTML = `Ta ruim hein?!`;
+        document.querySelector('.scorePct').style.color = '#ff0000'
+    } else if (points >= 50 && points < 70) {
+        document.querySelector('.scoreText1').innerHTML = 'Legal';
+        document.querySelector('.scorePct').style.color = '#ffff00';
+    } else if ( points >= 70) {
+        document.querySelector('.scoreText1').innerHTML = 'Parabéns!';
+        document.querySelector('.scorePct').style.color = '#0d630d'
+    }
+
+    document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`;
+    document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} questões e acertou ${correctAnswers}.`
+
+    document.querySelector('.scoreArea').style.display = 'block';
+    document.querySelector('.questionArea').style.display = 'none';
+    document.querySelector('.progress--bar').style.width = '100%';
+}
+
+function resetEvent() {
+    correctAnswers = 0;
+    currentQuestion = 0;
+    showQuestion();
 }
